b8592416cd6f5967ed523f42ec752d308d01e29c
Krypticality <143430684+Krypticality@users.noreply.github.com>
2023-10-30 16:11:54 +0000

    Tasks completed

--- domManipulation.js
+++ domManipulation.js
@@ -4,8 +4,10 @@ let image1Button = document.querySelector('.image-1-button');
 let image2Button = document.querySelector('.image-2-button');
 
 // html content elements
-let para = document.querySelector('.paragraph');
+let para = document.querySelector('p');
 let image = document.querySelector('.image');
+let name = document.querySelector('.name');
+let place = document.querySelector('.location');
 
 // CODE ALONG
 // 1. An event listener in provided for the `contentButton`
@@ -18,25 +20,27 @@ let image = document.querySelector('.image');
 //   - The `hobby` paragraph.
 //   - The `location` paragraph.
 
-contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+contentButton.addEventListener("click", function() {
+  name.innerHTML = "Diego";
+para.innerHTML = "I like to draw";
+place.innerHTML = "Chicago";
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
-
+image1Button.addEventListener('click', function () {
+  image.src = "rocket.gif";  
+});
 
 
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
 
+image2Button.addEventListener('click', function () {
+  image.src = "train.gif";
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
